106fa8e6df3d18e1f36d89463599e457f4fe9776
RICHARD Sullivan <sullivan.dono@gmail.com>
2012-10-13 22:35:24 +0200

    Ajout BDD

---
+++ message.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Client: localhost
+-- Généré le: Sam 13 Octobre 2012 à 20:32
+-- Version du serveur: 5.5.24-log
+-- Version de PHP: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données: `message`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `membre`
+--
+
+CREATE TABLE IF NOT EXISTS `membre` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `mail` varchar(50) DEFAULT NULL,
+  `login` varchar(50) DEFAULT NULL,
+  `pass` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=77 ;
+
+--
+-- Contenu de la table `membre`
+--
+
+INSERT INTO `membre` (`id`, `name`, `mail`, `login`, `pass`) VALUES
+(74, 'RICHARD', 'sullivan.dono@gmail.com', 'YoCsusu', 'b8655a8a445b5915e7ce35eabe3459036dee080f'),
+(75, 'CHARDONNAY', 'chardonnay.lisa@gmail.com', 'T0kiiT0', 'b26aad11c8223d18b81eec2de26568b09c3e0aea'),
+(76, 'TOURNIER', 'anthony.tournier25@gmail.com', 'syn', 'e656e58f7f38774baae3447adc0118ab2df0bacb');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `message`
+--
+
+CREATE TABLE IF NOT EXISTS `message` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `contact` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `telephone` varchar(45) NOT NULL,
+  `message` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `message`
+--
+
+INSERT INTO `message` (`id`, `date`, `contact`, `email`, `telephone`, `message`) VALUES
+(1, '2012-10-09 20:56:56', 'Sullivan', 'sullivan.dono@gmail.com', '0645567870', 'Hello !! ');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
